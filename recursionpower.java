0f4b315ba3d70c46c68da58baad7d7c69c40ef3f
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-26 14:29:52 +0530

    Added Recursion Power program

---
+++ recursionpower.java
@@ -0,0 +1,18 @@
+public class RecursionPower {
+
+    static int power(int base, int exponent) {
+
+        if (exponent == 0)
+            return 1;
+
+        return base * power(base, exponent - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(power(2, 4));
+
+    }
+
+}
