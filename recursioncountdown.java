45783b5355e1a16c2ab01e5d5fdf10fd202314fa
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-26 14:30:36 +0530

    Added Recursion Countdown program

---
+++ recursioncountdown.java
@@ -0,0 +1,20 @@
+public class RecursionCountdown {
+
+    static void countdown(int n) {
+
+        if (n == 0)
+            return;
+
+        System.out.println(n);
+
+        countdown(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        countdown(5);
+
+    }
+
+}
