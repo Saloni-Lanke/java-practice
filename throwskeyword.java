fba26d7a3e8f9f67b3275564cc4569f82d4403c2
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-28 23:14:18 +0530

    Added Throws Keyword program

---
+++ throwskeyword.java
@@ -0,0 +1,29 @@
+public class ThrowsKeyword {
+
+    static void checkAge(int age) throws Exception {
+
+        if (age < 18) {
+
+            throw new Exception("Not Eligible");
+
+        }
+
+        System.out.println("Eligible");
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            checkAge(16);
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+}
