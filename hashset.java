3a1650774622f29ec008bcb2d33d9a9bbcfadbb4
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-30 15:15:02 +0530

    Added HashSet program

---
+++ hashset.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+
+    public static void main(String[] args) {
+
+        HashSet<String> colors = new HashSet<>();
+
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Red");
+
+        System.out.println(colors);
+
+    }
+
+}
