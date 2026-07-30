69036ff114de1199bbc099d50da092b2a8805484
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-30 15:14:00 +0530

    Added LinkedList program

---
+++ linkedlist.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> names = new LinkedList<>();
+
+        names.add("Saloni");
+        names.add("Rahul");
+        names.add("Anjali");
+
+        System.out.println(names);
+
+    }
+
+}
