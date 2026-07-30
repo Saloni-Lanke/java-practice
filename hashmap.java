56ab651ff44672c79a94b3c62df434ac21130196
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-30 15:15:33 +0530

    Added HashMap program

---
+++ hashmap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> students = new HashMap<>();
+
+        students.put(1, "Saloni");
+        students.put(2, "Rahul");
+        students.put(3, "Anjali");
+
+        System.out.println(students);
+
+    }
+
+}
