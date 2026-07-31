e99814a1bcb53cf6c28e0453b4062d2cbbe0d473
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-31 20:13:39 +0530

    Added Vector program

---
+++ vector.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class VectorExample {
+
+    public static void main(String[] args) {
+
+        Vector<String> cities = new Vector<>();
+
+        cities.add("Pune");
+        cities.add("Mumbai");
+        cities.add("Nashik");
+
+        System.out.println(cities);
+
+    }
+
+}
