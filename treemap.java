448c78174ab328d73017c943ca2acb158af6ff94
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-07-31 20:09:52 +0530

    Added TreeMap program

---
+++ treemap.java
@@ -0,0 +1,17 @@
+import java.util.TreeMap;
+
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+
+        TreeMap<Integer, String> students = new TreeMap<>();
+
+        students.put(3, "Anjali");
+        students.put(1, "Saloni");
+        students.put(2, "Rahul");
+
+        System.out.println(students);
+
+    }
+
+}
