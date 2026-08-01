ca8a9362735d302985b75d33617fcb8c932d689d
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-01 22:55:38 +0530

    Added Synchronization program

---
+++ synchronization.java
@@ -0,0 +1,25 @@
+class Table {
+
+    synchronized void printTable(int n) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println(n * i);
+
+        }
+
+    }
+
+}
+
+public class Synchronization {
+
+    public static void main(String[] args) {
+
+        Table obj = new Table();
+
+        obj.printTable(5);
+
+    }
+
+}
