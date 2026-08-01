618025e99fea0341b8bcc8b969a54cfedd26d0be
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-01 22:54:39 +0530

    Added Thread Sleep program

---
+++ threadsleep.java
@@ -0,0 +1,23 @@
+public class ThreadSleep {
+
+    public static void main(String[] args) {
+
+        try {
+
+            for (int i = 1; i <= 5; i++) {
+
+                System.out.println(i);
+
+                Thread.sleep(1000);
+
+            }
+
+        } catch (InterruptedException e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
