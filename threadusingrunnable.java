33c9dd7046a87d49e644552fc4246646d732d401
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-01 22:53:56 +0530

    Added Runnable Thread program

---
+++ threadusingrunnable.java
@@ -0,0 +1,20 @@
+class MyRunnable implements Runnable {
+
+    public void run() {
+        System.out.println("Runnable thread is running...");
+    }
+
+}
+
+public class ThreadUsingRunnable {
+
+    public static void main(String[] args) {
+
+        MyRunnable obj = new MyRunnable();
+        Thread t1 = new Thread(obj);
+
+        t1.start();
+
+    }
+
+}
