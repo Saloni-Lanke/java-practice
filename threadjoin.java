022604aba015c57871cf16f484b4c20c03e827f3
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-01 22:55:12 +0530

    Added Thread Join program

---
+++ threadjoin.java
@@ -0,0 +1,29 @@
+class MyThread extends Thread {
+
+    public void run() {
+
+        for (int i = 1; i <= 3; i++) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}
+
+public class ThreadJoin {
+
+    public static void main(String[] args) throws Exception {
+
+        MyThread t1 = new MyThread();
+
+        t1.start();
+
+        t1.join();
+
+        System.out.println("Main thread finished.");
+
+    }
+
+}
