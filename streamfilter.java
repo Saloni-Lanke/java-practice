a45ec3093c1f16f2b68eb15cb90df57f237f5bcb
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-02 19:47:18 +0530

    Added Stream Filter program

---
+++ streamfilter.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamFilter {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
+
+        numbers.stream()
+               .filter(n -> n > 20)
+               .forEach(System.out::println);
+
+    }
+
+}
