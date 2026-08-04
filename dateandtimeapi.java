49a9fc4728dc6e2861a202d97d24ce4015e30179
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-04 22:09:19 +0530

    Added Date and Time API program

---
+++ dateandtimeapi.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateAndTimeAPI {
+
+    public static void main(String[] args) {
+
+        System.out.println("Date : " + LocalDate.now());
+
+        System.out.println("Time : " + LocalTime.now());
+
+    }
+
+}
