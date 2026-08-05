0312ae9f71aac43c5eda778076f22db1486afdaf
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-05 21:29:51 +0530

    Added BinaryOperator program

---
+++ binaryoperatorexample.java
@@ -0,0 +1,13 @@
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+
+    public static void main(String[] args) {
+
+        BinaryOperator<Integer> multiply = (a, b) -> a * b;
+
+        System.out.println(multiply.apply(5, 4));
+
+    }
+
+}
