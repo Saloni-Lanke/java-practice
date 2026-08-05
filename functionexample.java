0ca6cdcd30ac90e727c558eaf2c1772976916f47
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-05 21:28:13 +0530

    Added Function program

---
+++ functionexample.java
@@ -0,0 +1,13 @@
+import java.util.function.Function;
+
+public class FunctionExample {
+
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> square = n -> n * n;
+
+        System.out.println(square.apply(5));
+
+    }
+
+}
