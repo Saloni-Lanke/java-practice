00edafdadbebb5e14cde96d9127a050dbf6dd1e3
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-06 21:25:12 +0530

    Added Enum program

---
+++ enumexample.java
@@ -0,0 +1,19 @@
+enum Day {
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY
+
+}
+
+public class EnumExample {
+
+    public static void main(String[] args) {
+
+        Day today = Day.MONDAY;
+
+        System.out.println(today);
+
+    }
+
+}
