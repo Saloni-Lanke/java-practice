5465539007fe0fe610ea724f0012866df5636c27
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-06 21:25:46 +0530

    Added Record program

---
+++ recordexample.java
@@ -0,0 +1,13 @@
+record Student(String name, int age) {}
+
+public class RecordExample {
+
+    public static void main(String[] args) {
+
+        Student s = new Student("Saloni", 20);
+
+        System.out.println(s);
+
+    }
+
+}
