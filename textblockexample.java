59387825e8f719ba257799bfd146bdb846bc6a98
Saloni-Lanke <saloni.cs23173@mmcc.edu.in>
2026-08-06 21:26:56 +0530

    Added Text Block program

---
+++ textblockexample.java
@@ -0,0 +1,15 @@
+public class TextBlockExample {
+
+    public static void main(String[] args) {
+
+        String text = """
+                Welcome
+                to
+                Java
+                """;
+
+        System.out.println(text);
+
+    }
+
+}
